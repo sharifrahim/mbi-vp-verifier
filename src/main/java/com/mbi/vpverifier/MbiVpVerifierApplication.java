d69b613651802731c2924064268443bf05725bca
sharifrahim <sharif.rahim@myeg.com.my>
2025-09-04 14:51:34 +0800

    Add Maven project structure and configuration files

---
+++ src/main/java/com/mbi/vpverifier/MbiVpVerifierApplication.java
@@ -0,0 +1,13 @@
+package com.mbi.vpverifier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbiVpVerifierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbiVpVerifierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mbi/vpverifier/MbiVpVerifierApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbi.vpverifier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbiVpVerifierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
